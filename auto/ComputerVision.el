0c49509563597e91fc5547e5af840ef700738d0a
Jinxiaohai <xiaohaijin@outlook.com>
2018-09-05 14:05:15 +0800

    数据结构--共生矩阵

--- auto/ComputerVision.el
+++ auto/ComputerVision.el
@@ -3,24 +3,25 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("ctexbook" "a4paper" "openany" "12pt")))
-   (add-to-list 'LaTeX-verbatim-environments-local "cppcode*")
-   (add-to-list 'LaTeX-verbatim-environments-local "cppcode")
-   (add-to-list 'LaTeX-verbatim-environments-local "minted")
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-environments-local "cppcode")
+   (add-to-list 'LaTeX-verbatim-environments-local "cppcode*")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "setup/setup"
     "图像及其表达与性质/图像及其表达与性质"
     "图像及其数学与物理背景/图像及其数学与物理背景"
+    "图像分析的数据结构/图像分析的数据结构"
     "OpenCV大数组类型/OpenCV大数组类型"
     "Mat2QImage/Mat2QImage"
     "ctexbook"
